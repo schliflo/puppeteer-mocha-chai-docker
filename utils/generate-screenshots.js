1cf6546b405a87e13e95e410ce7ce5656939829b
Florian Schlittenbauer <florian.schlittenbauer@gmail.com>
2018-04-19 13:53:37 +0200

    add a little bit of feedback for screenshot generation

--- utils/generate-screenshots.js
+++ utils/generate-screenshots.js
@@ -4,13 +4,14 @@ const urls = process.env.URLS.split(',');
 urls.forEach(async (url) => {
     const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-dev-shm-usage']});
     const page = await browser.newPage();
-    await page.setViewport({width: 1280, height: 720});
 
-    await page.goto(url, {'waitUntil': 'networkidle0'});
+    await page.setViewport({width: 1280, height: 720});
+    await page.goto(url, {'waitUntil': 'networkidle0'}).then(()=>{
+        console.log('Generating screeshot for ' + url);
+    });
     await page.screenshot({
         fullPage: true,
         path: '/validator/screenshots/' + url.replace(/^.*\/\/[^\/]+/, '').replace(/(\.html)|(\.htm)|(\.php)/, '') + '.png'
     });
-
     await browser.close();
 });
